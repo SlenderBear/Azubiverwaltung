9f7b6748a76ee69a681b5ba3971299c5197e096d
dunkel.gregor <dunkel.gregor@A123-PC17.HERTZ.local>
2015-02-04 11:39:13 +0100

    sqliteconnector erstellt.

--- src/datenbank/SqliteConnector.java
+++ src/datenbank/SqliteConnector.java
@@ -1,5 +1,104 @@
 package datenbank;
 
-public class SqliteConnector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-}
+/**
+ * @author dunkel.gregor
+ * 
+ */
+public class SqliteConnector {
+	
+	private static SqliteConnector connector;
+	private static Connection con;
+
+	/**
+	 * Private da Singleton.
+	 */
+	private SqliteConnector() {
+	}
+	
+	/**
+	 * Liefert die Instanz des {@link SqliteConnector}s.
+	 * @return
+	 */
+	public SqliteConnector getInstance() {
+		if(connector == null) {
+			connector = new SqliteConnector();
+		}
+		return connector;
+	}
+	
+	/**
+	 * Baut eine Verbindung zur Datenbank auf.
+	 * @return {@link Connection}
+	 */
+	private Connection getConnection() {
+		 Connection c = null;
+		 if(con == null)
+		 {
+		    try {
+		      Class.forName("org.sqlite.JDBC");
+		      c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		    } catch ( Exception e ) {
+		    	throw new RuntimeException(this.getClass() + ":" + e.getMessage());
+		    }
+		 }
+		 return c;
+	}
+
+	/**
+	 * Führt den übergebenen Select Befehl aus.
+	 * @param sql String
+	 * @return {@link ResultSet} Ergebnisse.
+	 */
+	public ResultSet executeQuery(String sql) {
+		Connection c = this.getConnection();
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			c.setAutoCommit(false);
+			System.out.println("Opened database successfully");
+
+			stmt = c.createStatement();
+			rs = stmt.executeQuery(sql);
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		return rs;
+	}
+	
+	/**
+	 * Führt den übergebenen Sql-Befehl aus.
+	 * @param sql String
+	 * @return true wenn erfolgreich.
+	 */
+	public boolean statementExecute(String sql) {
+	    Connection c = this.getConnection();
+	    Statement stmt = null;
+	    try {
+	      Class.forName("org.sqlite.JDBC");
+	      c = DriverManager.getConnection("jdbc:sqlite:test.db");
+	      c.setAutoCommit(false);
+	      System.out.println("Opened database successfully");
+
+	      stmt = c.createStatement();
+	      if(stmt.executeUpdate(sql) > 0) {
+	    	  c.commit();
+		      stmt.close();
+		      c.close();
+	    	  return true;
+	      }
+
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      System.exit(0);
+	    }
+		return false;
+	}
+ }
