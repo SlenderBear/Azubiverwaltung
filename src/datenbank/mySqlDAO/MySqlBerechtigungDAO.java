16154f8a92919799f69fc27578296bc86a5e5ce4
mertmann.justin <mertmann.justin@A123-PC18.HERTZ.local>
2015-02-04 11:58:32 +0100

    Note DAO eingefügt und logik entfernt aus berechtigungDAO Änderung Berechtigungsid = int

--- src/datenbank/mySqlDAO/MySqlBerechtigungDAO.java
+++ src/datenbank/mySqlDAO/MySqlBerechtigungDAO.java
@@ -12,29 +12,18 @@ public class MySqlBerechtigungDAO implements StandardMySqlDAO<Berechtigung>{
 
 	@Override
 	public Berechtigung insert(Berechtigung t) {
-		String guid = MySQLConnector.getInstance().getNewGUID();
-		String sql = "INSERT INTO berechtigung values(" 
-				+ guid 
-				+ ",'" + t.getBezeichnung() 
-				+ ")";
-		MySQLConnector.getInstance().statementExecute(sql);
-		t.setID(guid);
 		return t;
 	}
 
 	@Override
 	public boolean update(Berechtigung t) {
-		String sql = "UPDATE berechtigung"+
-				"SET bezeichnung="+t.getBezeichnung()+
-				" WHERE berechtigungid="+t.getID()+";";
-		return MySQLConnector.getInstance().statementExecute(sql);
+
+		return false;
 	}
 
 	@Override
 	public boolean delete(Berechtigung t) {
-		String sql = "delete from berechtigung"+
-				" WHERE berechtigungid="+t.getID()+";";
-		return MySQLConnector.getInstance().statementExecute(sql);
+		return false;
 	}
 
 	@Override
@@ -46,7 +35,7 @@ public class MySqlBerechtigungDAO implements StandardMySqlDAO<Berechtigung>{
 		 while (rs.next())
 	      {
 			 Berechtigung b = new Berechtigung();
-	        b.setID(rs.getString("berechtigungid"));
+	        b.setID(rs.getInt("berechtigungid"));
 	        b.setBezeichnung(rs.getString("bezeichnung"));
 	       
 	        berechtigungListe.add(b);
@@ -64,7 +53,7 @@ public class MySqlBerechtigungDAO implements StandardMySqlDAO<Berechtigung>{
 		Berechtigung b = new Berechtigung();
 		try {
 			rs.next();
-	        b.setID(rs.getString("berechtigungid"));
+	        b.setID(rs.getInt("berechtigungid"));
 	        b.setBezeichnung(rs.getString("bezeichnung"));
 		} catch (SQLException e) {
 			e.printStackTrace();
--- src/datenbank/mySqlDAO/MySqlNoteDAO.java
+++ src/datenbank/mySqlDAO/MySqlNoteDAO.java
@@ -1,9 +1,52 @@
 package datenbank.mySqlDAO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import objects.Berechtigung;
+import objects.Note;
 import datenbank.MySQLConnector;
 import datenbank.StandardMySqlDAO;
 
-public class MySqlNoteDAO {
-	private MySQLConnector connector = new MySQLConnector();
+public class MySqlNoteDAO implements StandardMySqlDAO<Note>{
+
+	@Override
+	public Note insert(Note t) {
+		return t;
+	}
+
+	@Override
+	public boolean update(Note t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Note t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<Note> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Note getByGuid(String beschreibung) {
+		String sql = "select * from note where beschreibung="+beschreibung+"";
+		ResultSet rs = MySQLConnector.getInstance().executeQuery(sql);
+		Note b = new Note();
+		try {
+			rs.next();
+	        b.setZahl(rs.getInt("zahl"));
+	        b.setBeschreibung((rs.getString("beschreibung")));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return b;
+	}
 
 }
