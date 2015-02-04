63dc2f927471d127061395a9af05a270aeff9fb0
imaev.maksim <imaev.maksim@A123-PC21.HERTZ.local>
2015-02-04 09:21:02 +0100

    Zeugnisverwaltung

---
+++ src/gui/MyTable.java
@@ -0,0 +1,27 @@
+package gui;
+
+import javax.swing.table.DefaultTableModel;
+
+public class MyTable extends DefaultTableModel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public MyTable(int row, int column) {
+		super(row, column);
+	}
+	@Override
+	public boolean isCellEditable(int row, int column) {
+		switch (column) {
+		case 0:
+			return false;
+		case 1:
+			return true;
+		default:
+			return true;
+		}
+		
+	}
+}
