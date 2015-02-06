7288932f918337030d6b22a7f3b776775e8bc1de
dunkel.gregor <dunkel.gregor@A123-PC19.HERTZ.local>
2015-02-06 11:16:04 +0100

    sqlite

---
+++ src/datenbank/SqliteDAO/SqliteBetriebDAO.java
@@ -0,0 +1,52 @@
+package datenbank.SqliteDAO;
+
+import java.util.ArrayList;
+
+import objects.Betrieb;
+import datenbank.StandardDAO;
+
+public class SqliteBetriebDAO implements StandardDAO<Betrieb>{
+
+	@Override
+	public Betrieb insert(Betrieb t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(Betrieb t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Betrieb t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<Betrieb> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Betrieb getByGuid(String guid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isVorhanden(Betrieb t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getClassName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
