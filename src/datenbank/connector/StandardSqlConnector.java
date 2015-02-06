787e99b25468a362ecc943431121aa353ea39f2b
dietz.philipp <dietz.philipp@A123-PC17.HERTZ.local>
2015-02-06 14:35:11 +0100

    Comments

--- src/datenbank/connector/StandardSqlConnector.java
+++ src/datenbank/connector/StandardSqlConnector.java
@@ -2,11 +2,30 @@ package datenbank.connector;
 
 import java.sql.ResultSet;
 
+/**
+ * Stellt die StandardMethoden, die für einen SQL-Connector benötigt werden, dar.
+ * @author dietz.philipp
+ *
+ */
 public interface StandardSqlConnector {
 	
+	/**
+	 * Führt ein Insert/Update/Delete aus.
+	 * @param sql String
+	 * @return true wenn erfolgreich.
+	 */
 	boolean statementExecute(String sql);
 	
+	/**
+	 * Liefert die Objekte zum Query.
+	 * @param sql String
+	 * @return {@link ResultSet}
+	 */
 	ResultSet executeQuery(String sql);
 	
+	/**
+	 * Erzeugt eine GUID.
+	 * @return Guid/UUID String
+	 */
 	String getNewGUID();
 }
