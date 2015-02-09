1e4551d30b158ec59df00efc373a0fb7f48cf969
SlenderBear <thegreg1602@gmail.com>
2015-02-09 19:56:46 +0100

    komstanten

---
+++ src/datenbank/Konstanten.java
@@ -0,0 +1,9 @@
+package datenbank;
+
+public interface Konstanten {
+
+	/** Konstante für die Property SQLite-Pfad. */
+	public static final String sqlite_path = "sqlite_path";
+	/** Konstante für die Property MySQL-Pfad. */
+	public static final String mysql_path = "mysql_path";
+}
